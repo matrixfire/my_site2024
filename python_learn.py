3efb995b79d3f96abd84b4b34bce7d8524c084df
matrixfire <48238435+matrixfire@users.noreply.github.com>
2024-02-26 16:08:58 +0800

    ok

---
+++ python_learn.py
@@ -0,0 +1,18 @@
+def main():
+	n = int(input("What's n? "))
+	for s in sheep(n):
+		print(s)
+
+def sheep(n):
+	flock = []
+	for i in range(n):
+		flock.append("O"*i)
+	return flock
+
+def sheep2(n):
+	for i in range(n):
+		yield "O"*i
+
+if __name__ == '__main__':
+	main()
+	# pass
